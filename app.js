2d5145ee299eda0ea78685e0753ce79a13124477
Alexveme <62341832+Alexveme@users.noreply.github.com>
2020-06-09 12:11:09 +0200

    url update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import blogRouter from './blogRouter.js';
 
 const app = express()
 
-const mongoUrl = 'mongodb+srv://prueba:prueba@cluster0-cg3jl.mongodb.net/notasdb?retryWrites=true&w=majority'
+const mongoUrl = 'mongodb+srv://prueba:prueba@cluster0-cg3jl.mongodb.net/blogsdb?retryWrites=true&w=majority'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
